6345c545daf0dd6455fcae6e3c481fa35a2066c2
LucaBenjaminGSMA <lbenjamin@gsma.com>
2024-07-30 15:02:33 +0100

    Update LogoChanger.js

--- JS/LogoChanger.js
+++ JS/LogoChanger.js
@@ -1,7 +1,6 @@
+let image_url = ""
 
-var image_url = "https://media.licdn.com/dms/image/D4E0BAQFoLemWOmOC8w/company-logo_200_200/0/1707325937032/widefglobal_logo?e=2147483647&v=beta&t=nJSi7b-gRQM1a5lfXUnIK2wGZ3eC7Vkv2j8n7vGh04Q";
-
-if($.fluxx.config.user_profile_name != "Grantee")
+if($.fluxx.config.user_profile_name == "WiDEF_Grantee")
 {
 image_url = "https://media.licdn.com/dms/image/D4E0BAQFoLemWOmOC8w/company-logo_200_200/0/1707325937032/widefglobal_logo?e=2147483647&v=beta&t=nJSi7b-gRQM1a5lfXUnIK2wGZ3eC7Vkv2j8n7vGh04Q"
 }
@@ -9,3 +8,5 @@ else
 {
 image_url = "https://www.gsma.com/wp-content/themes/theme_gsma_2023/images_2023/GSMA-Logo-Black-RGB.svg"
 }
+
+document.getElementById("logo").children[0].src = image_url
