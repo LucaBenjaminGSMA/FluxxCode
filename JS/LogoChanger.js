78abaf0447a52dff9a7568b73e08ecebc11299d5
LucaBenjaminGSMA <lbenjamin@gsma.com>
2024-07-30 14:46:46 +0100

    Create LogoChanger.js

---
+++ JS/LogoChanger.js
@@ -0,0 +1,11 @@
+
+var image_url = "https://media.licdn.com/dms/image/D4E0BAQFoLemWOmOC8w/company-logo_200_200/0/1707325937032/widefglobal_logo?e=2147483647&v=beta&t=nJSi7b-gRQM1a5lfXUnIK2wGZ3eC7Vkv2j8n7vGh04Q";
+
+if($.fluxx.config.user_profile_name != "Grantee")
+{
+image_url = "https://media.licdn.com/dms/image/D4E0BAQFoLemWOmOC8w/company-logo_200_200/0/1707325937032/widefglobal_logo?e=2147483647&v=beta&t=nJSi7b-gRQM1a5lfXUnIK2wGZ3eC7Vkv2j8n7vGh04Q"
+}
+else
+{
+image_url = "https://www.gsma.com/wp-content/themes/theme_gsma_2023/images_2023/GSMA-Logo-Black-RGB.svg"
+}
